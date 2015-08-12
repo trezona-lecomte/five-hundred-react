7761ba89328c3b81acf1ba0f10e8368992c51186
Kieran Trezona-le Comte <trezona-lecomte@gmail.com>
2015-08-13 10:11:55 +1200

    Update RoundPage to show activeTrick & hand

--- scripts/components/rounds/RoundPage.react.jsx
+++ scripts/components/rounds/RoundPage.react.jsx
@@ -3,6 +3,7 @@ var State               = require('react-router').State;
 var WebAPIUtils         = require('../../utils/WebAPIUtils.js');
 var RoundStore          = require('../../stores/RoundStore.react.jsx');
 var RoundActionCreators = require('../../actions/RoundActionCreators.react.jsx');
+var ErrorNotice         = require('../common/ErrorNotice.react.jsx');
 
 var RoundPage = React.createClass({
 
@@ -26,6 +27,7 @@ var RoundPage = React.createClass({
 
   _onChange: function() {
     console.log("Got round from store: " + RoundStore.getRound());
+    console.log("Got round errors from store: " + RoundStore.getErrors());
     this.setState({
       round: RoundStore.getRound(),
       errors: RoundStore.getErrors()
@@ -33,35 +35,21 @@ var RoundPage = React.createClass({
   },
 
   render: function() {
-    console.log("ROUND: " + JSON.stringify(this.state.round));
+    console.log('re-rendering: ' + this.state.errors.length);
+    var errors = (this.state.errors.length > 0) ? <ErrorNotice errors={this.state.errors}/> : <div></div>;
+    var round = this.state.round;
+    console.log("ROUND: " + JSON.stringify(round));
     return (
-      <div className="row">
-          <div className="round-id"><h2>Round {this.state.round.id}</h2></div>
-          <div className="round-tricks">Number of tricks: {this.state.round.tricks.length}</div>
-          <TricksList tricks={this.state.round.tricks} />
-          <PlayerCardsList playerCards={this.state.round.player_cards} />
+      <div>
+          {errors}
+          <div className="row">
+              <div className="round-id"><h2>Round {round.id}</h2></div>
+              <div className="round-tricks">Number of tricks: {round.trick_ids.length}</div>
+              <ActiveTrick trick={round.active_trick} />
+              <PlayerCardsList currentPlayerCards={round.current_player_cards} round={round} />
+          </div>
       </div>
     );
-  }
-});
-
-var TrickItem = React.createClass({
-  render: function() {
-    console.log('trick_cards: ' + JSON.stringify(this.props.trick));
-    return (
-      <div className="trick panel">
-          <div className="trick-id">
-              <p>Trick {this.props.trick.id}</p>
-          </div>
-          <div>
-              <ul className="trick-cards">
-                  {this.props.trick.cards.map(function(card, index){
-                    return <TrickCardItem card={card} key={"card-" + index} />
-                   })}
-              </ul>
-          </div>
-      </div>
-    )
   }
 });
 
@@ -82,23 +70,53 @@ var TricksList = React.createClass({
   }
 });
 
-var TrickCardItem  = React.createClass({
+var ActiveTrick = React.createClass({
   render: function() {
-    console.log('trick-card-item: ' + this.props.card);
     return (
-      <li className="trick-card">
-          <p>{this.props.card.rank} of {this.props.card.suit} - {this.props.card.played_by}</p>
-      </li>
-    );
+      <div className="active-trick panel">
+          <div className="trick-number">
+              <h4>Trick {this.props.trick.number_in_round} (current winning card: {this.props.trick.winning_card_id})</h4>
+          </div>
+          <div>
+              <TrickCardsList trickCards={this.props.trick.cards} />
+          </div>
+      </div>
+    )
   }
 });
 
-var PlayerCardItem  = React.createClass({
+var TrickCardsList = React.createClass({
+  getDefaultProps: function() {
+    return {
+      trickCards: []
+    }
+  },
   render: function() {
-    console.log('player-card-item: ' + this.props.card);
+    var cards = this.props.trickCards;
+    if (typeof cards !== 'undefined') {
+      cards = cards.sort(function(a, b) {
+        return a.position_in_trick - b.position_in_trick;
+      });
+    }
     return (
-      <li className="player-card">
-          <p>{this.props.card.rank} of {this.props.card.suit}</p>
+      <div className="trick-cards-list panel">
+          <div>
+              <ul className="trick-cards">
+                  {cards.map(function(card, index){
+                    return <TrickCardItem card={card} key={"card-" + index} />
+                   })}
+              </ul>
+          </div>
+      </div>
+    )
+  }
+});
+
+var TrickCardItem  = React.createClass({
+  render: function() {
+    return (
+      <li className="trick-card">
+          <p>Card #{this.props.card.id} | {this.props.card.rank} of {this.props.card.suit} - {this.props.card.played_by}</p>
       </li>
     );
   }
@@ -107,11 +125,13 @@ var PlayerCardItem  = React.createClass({
 var PlayerCardsList = React.createClass({
   getDefaultProps: function() {
     return {
+      round: {},
       playerCards: []
     }
   },
   render: function() {
-    console.log('player_cards: ' + JSON.stringify(this.props.playerCard));
+    console.log('player_cards: ' + JSON.stringify(this.props.playerCards));
+    var round = this.props.round;
     return (
       <div className="player-cards panel">
           <div className="player-cards-heading">
@@ -119,13 +139,38 @@ var PlayerCardsList = React.createClass({
           </div>
           <div>
               <ul className="player-cards">
-                  {this.props.playerCards.map(function(card, index){
-                    return <PlayerCardItem card={card} key={"card-" + index} />
+                  {this.props.currentPlayerCards.map(function(card, index){
+                    return <PlayerCardItem card={card} round={round} key={"card-" + index} />
                    })}
               </ul>
           </div>
       </div>
     )
+  }
+});
+
+var PlayerCardItem  = React.createClass({
+  getDefaultProps: function() {
+    return {
+      round: {},
+      card: {}
+    }
+  },
+  playCard: function(e) {
+    e.preventDefault();
+    RoundActionCreators.playCard(this.props.round, this.props.card.id);
+  },
+  render: function() {
+    roundId = this.props.roundId
+    card = this.props.card
+    console.log('player-card-item: ' + this.props.card);
+    return (
+      <li className="player-card">
+          <button onClick={this.playCard}>
+              {card.rank} of {card.suit}
+          </button>
+      </li>
+    );
   }
 });
 
